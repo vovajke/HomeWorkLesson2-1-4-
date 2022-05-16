182ccd01a12dd3480a5ed01f006977e5823b02ed
vovajke <102599169+vovajke@users.noreply.github.com>
2022-05-16 18:21:07 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,30 +6,21 @@ namespace HomeWorkLesson2_1_4_
     {
 		static void Main(string[] args)
 		{
-			Greet();
-			Menu();
+			Welcome();
+			Consol();
 			Console.ReadKey();
 
 		}
-		static void Greet()
+		static void Welcome()
 		{
 			DateTime date = DateTime.Now;
 			Console.WriteLine($"Привет, VladimirGerasimov, сегодня {date}");
 			Console.Write("\n");
 		}
 
-		static void MenuPresent()
-		{
-			Console.WriteLine("=================================");
-			Console.WriteLine(" T - cреднесуточная  температура ");
-			Console.WriteLine(" M - Вывести название ук. месяца ");
-			Console.WriteLine(" N - проверить число на четность ");
-			Console.WriteLine(" R - Будет ли эта зима дождливой ");
-			Console.WriteLine(" C-  Вывести  данные с касс. чека");
-			Console.WriteLine("=================================");
-		}
 
-		static void Temp()
+
+        static void Temp()
 		{
 
 			try
@@ -52,79 +43,47 @@ namespace HomeWorkLesson2_1_4_
 		}
 		static void MntName()
 		{
-			try
-			{
-				Console.Write("Введите порядковый номер месяца: ");
-				int number = Convert.ToInt32(Console.ReadLine());
-				switch (number)
-				{
-					case 1:
-						Console.WriteLine("Январь");
-						break;
-					case 2:
-						Console.WriteLine("Февраль");
-						break;
-					case 3:
-						Console.WriteLine("Март");
-						break;
-					case 4:
-						Console.WriteLine("Апрель");
-						break;
-					case 5:
-						Console.WriteLine("Май");
-						break;
-					case 6:
-						Console.WriteLine("Июнь");
-						break;
-					case 7:
-						Console.WriteLine("Июль");
-						break;
-					case 8:
-						Console.WriteLine("Август");
-						break;
-					case 9:
-						Console.WriteLine("Сентябрь");
-						break;
-					case 10:
-						Console.WriteLine("Октябрь");
-						break;
-					case 11:
-						Console.WriteLine("Ноябрь");
-						break;
-					case 12:
-						Console.WriteLine("Декабрь");
-						break;
-					default:
-						Console.WriteLine("такого месяца нет.");
-						break;
-				}
-			}
-			catch
-			{
-				Console.WriteLine("Извините, месяц не распознан.");
-			}
+			Console.WriteLine("Введи номер месяца");
+			Console.WriteLine($"{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}");
+		int n = int.Parse(Console.ReadLine()); 
+			if (n == 1)
+				Console.WriteLine("Месяц под номером " + n + " : Январь");
+			if (n == 2)
+				Console.WriteLine("Месяц под номером " + n + " : Февраль");
+			if (n == 3)
+				Console.WriteLine("Месяц под номером " + n + " : Март");
+			if (n == 4)
+				Console.WriteLine("Месяц под номером " + n + " : Апрель");
+			if (n == 5)
+				Console.WriteLine("Месяц под номером " + n + " : Май");
+			if (n == 6)
+				Console.WriteLine("Месяц под номером " + n + " : Июнь");
+			if (n == 7)
+				Console.WriteLine("Месяц под номером " + n + " : Июль");
+			if (n == 8)
+				Console.WriteLine("Месяц под номером " + n + " : Август");
+			if (n == 9)
+				Console.WriteLine("Месяц под номером " + n + " : Сентябрь");
+			if (n == 10)
+				Console.WriteLine("Месяц под номером " + n + " : Октябрь");
+			if (n == 11)
+				Console.WriteLine("Месяц под номером " + n + " : Ноябрь");
+			if (n == 12)
+				Console.WriteLine("Месяц под номером " + n + " : Декабрь");
 
-		}
-		static void Cheque()
-		{
-			string date = "01.01.22 00:19";
-			int cmena = 3;
-			int check = 5;
-			string kassir = "СИСТ.АДМИН";
-			string place = "Магазин №72";
-			string service = "1.000 X 2000.00";
-			double total = 2000.00;
-			double cash = 2000.00;
-			Console.WriteLine($"{date}");
-			Console.WriteLine($"СМЕНА: {cmena} ЧЕК: {check}");
-			Console.WriteLine($"КАССИР: {kassir}");
-			Console.WriteLine($"МЕСТО РАСЧЕТОВ: {place}");
-			Console.WriteLine($"плата за услуги телефона - {service}");
-			Console.WriteLine($"ИТОГ: ={total}");
-			Console.WriteLine("ПОЛУЧЕНО:");
-			Console.WriteLine($"НАЛИЧНЫМИ: {cash}");
+			if (n == 13)
+				Console.WriteLine("такого месяца нет");
+			
+			    Console.ReadLine();
+			
 
+		
+			
+			
+			
 		}
+
+
 		static bool UtilEven(int x)
 		{
 			if ((x % 2) != 0)
@@ -144,17 +103,37 @@ namespace HomeWorkLesson2_1_4_
 				int number = Convert.ToInt32(Console.ReadLine());
 				if (UtilEven(number) == false)
 				{
-					Console.Write("Данное число является нечетным.");
+					Console.Write("Это число является нечетным.");
 				}
 				else
 				{
-					Console.Write("Данное число является четным.");
+					Console.Write("Это число является четным.");
 				}
 			}
 			catch
 			{
-				Console.Write("Пожалуйста, введите число.\n");
+				Console.Write(" введите число.\n");
 			}
+		}
+		static void Cheque()
+		{
+			string date = "01.01.22 00:19";
+			int cmena = 3;
+			int check = 5;
+			string kassir = "Кассовый Работник";
+			string place = "Магазин №72";
+			string service = "1.000 X 2000.00";
+			double total = 2000.00;
+			double cash = 2000.00;
+			Console.WriteLine($"{date}");
+			Console.WriteLine($"СМЕНА: {cmena} ЧЕК: {check}");
+			Console.WriteLine($"КАССИР: {kassir}");
+			Console.WriteLine($"МЕСТО РАСЧЕТОВ: {place}");
+			Console.WriteLine($"плата за услуги телефона + Интр. - {service}");
+			Console.WriteLine($"ИТОГ: ={total}");
+			Console.WriteLine("ПОЛУЧЕНО:");
+			Console.WriteLine($"НАЛИЧНЫМИ: {cash}");
+
 		}
 
 		static void WntRain()
@@ -186,7 +165,7 @@ namespace HomeWorkLesson2_1_4_
 			}
 			catch
 			{
-				Console.Write("Пожалуйста, введите число.\n");
+				Console.Write(" введите число.\n");
 
 			}
 
@@ -194,7 +173,7 @@ namespace HomeWorkLesson2_1_4_
 		}
 
 
-		static void Menu()
+		static void Consol()
 		{
 			MenuPresent();
 			string input = Console.ReadLine();
@@ -217,10 +196,24 @@ namespace HomeWorkLesson2_1_4_
 					break;
 				default:
 					Console.WriteLine("Ошибка проверьте вводимые данные.");
-					Menu();
+					Consol();
 					break;
 
 			}
+			static void MenuPresent()
+			{
+				Console.WriteLine("=================================" );
+				Console.WriteLine("- T - cреднесуточная  температура " );
+				Console.WriteLine("- M - Выввод названия ук. месяца " );
+				Console.WriteLine("- N - проверка чисел на четность " );
+				Console.WriteLine("- R - Будет ли эта зима дождливой " );
+				Console.WriteLine("- C - Вывести  данные с касс. чека ");
+				Console.WriteLine("=================================" );
+			}
+
+
+
+
 		}
 	}
 
